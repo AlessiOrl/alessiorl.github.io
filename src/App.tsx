6ea40166bc3ca34dfc17e6b3b9aeed8e6dacbf7f
AlessiOr <orlandoalessio1998@gmail.com>
2026-04-02 16:42:38 +0200

    refactor: remove ThemeProvider and related context, update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { ThemeProvider } from './context/ThemeContext'
 import { NoiseOverlay } from './components/ui/NoiseOverlay'
 import { HeroSection } from './components/sections/HeroSection'
 
 function App() {
   return (
-    <ThemeProvider>
+    <>
       <NoiseOverlay />
-
       <main>
         <HeroSection />
       </main>
-    </ThemeProvider>
+    </>
   )
 }
 
